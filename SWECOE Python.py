37e2e35643f83fc0851ac8f40de6f1934c9af856
isthisausername125 <67210594+isthisausername125@users.noreply.github.com>
2020-06-21 11:25:10 -0500

    Add files via upload
    Development prototype. I suck at programming.

---
+++ SWECOE Python.py
@@ -0,0 +1,22 @@
+import sys
+
+# Main Menu Script
+print("SWECOE Prototype. 6/21/2020.")
+m = input("Enter the 1 key for more info. Enter any other key to exit.")
+if m == "1":
+    x = input("""Main Menu
+1 = Calculator
+2 = About""")
+# Menu Option Scripts
+    if x == "1":
+        print("test.")
+    if x == "2":
+        print("test 2.")
+#Quit Script
+else:
+    sys.exit(0)
+
+
+
+    
+    
